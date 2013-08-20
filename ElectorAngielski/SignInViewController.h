5eddc0f24702c932a8aafc51549febc9d4c3c61e
Michal Ziobro <michzio@oscil249.ct.infn.it>
2013-08-20 09:46:25 +0200

    First App Commit

---
+++ ElectorAngielski/SignInViewController.h
@@ -0,0 +1,13 @@
+//
+//  SignInViewController.h
+//  ElectorAngielski
+//
+//  Created by Michal Ziobro on 20/08/2013.
+//  Copyright (c) 2013 Michal Ziobro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SignInViewController : UIViewController
+
+@end
